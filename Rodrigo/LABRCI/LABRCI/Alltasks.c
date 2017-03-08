b910cc0b7a8b33e31a34a374e91c731086e4eaca
Rodrigo Lopes Do Ó Barbosa <rodrigo.do.o.barbosa@gmail.com>
2017-03-08 18:02:34 +0000

    entrou ficheiro c

--- Rodrigo/LABRCI/LABRCI/Alltasks.c
+++ Rodrigo/LABRCI/LABRCI/Alltasks.c
@@ -6,10 +6,99 @@
 //  Copyright (c) 2017 Rodrigo Lopes Do Ó Barbosa. All rights reserved.
 //
 
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <errno.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <string.h>
+# include <unistd.h>
+extern int errno;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+int main(void){
+    
+    // gethostname
+    /*char buffer[128];
+     
+     if (gethostname(buffer, 128)==-1) {
+     printf("error: %s\n", strerror(errno));
+     }else{
+     printf("host name: %s\n", buffer);
+     exit (0);
+     }*/
+    
+    //gethostbyname
+    
+    struct hostent *h;
+    struct in_addr *a;
+    
+    if ((h = gethostbyname("macbook-de-rodrigo-lopes-do-o-barbosa-2.local"))==NULL) {
+        printf("error: %s\n", strerror(errno));
+        exit(1);
+    }
+    
+    printf("official host name: %s\n", h->h_name);
+    
+    a=(struct in_addr*)h->h_addr_list[0];
+    
+    printf("internet adress: %s (%08lX)\n", inet_ntoa(*a), (long unsigned int)ntohl(a->s_addr));
+    exit(0);
+    
+    //UDP Conection
+    /*
+     int fd, n, addrlen;
+     char buffer[128];
+     struct sockaddr_in addr;
+     struct hostent *h;
+     struct in_addr *a;
+     
+     
+     // struct in_addr iaddr;
+     
+     //inet_aton("193.136.138.142",&iaddr);
+     
+     if ((h = gethostbyname("tejo"))==NULL) {    //GET HOST NAME
+     printf("error: %s\n", strerror(errno));
+     exit(1);
+     }
+     
+     a=(struct in_addr*)h->h_addr_list[0];
+     
+     
+     
+     fd=socket(AF_INET,SOCK_DGRAM,0); //UDP SOCKET
+     
+     if(fd==-1){
+     printf("error: %s\n", strerror(errno));
+     exit(1);
+     }
+     
+     memset((void*)&addr, (int)'\0', sizeof(addr));
+     addr.sin_family=AF_INET;
+     addr.sin_addr= *a;
+     addr.sin_port=htons(58000);
+     
+     n=sendto(fd,"Cheguei!\n",9,0,(struct sockaddr*)&addr, sizeof(addr));
+     if(n==-1){
+     printf("error: %s\n", strerror(errno));
+     exit(1);
+     }
+     
+     addrlen=sizeof(addr);
+     
+     n= recvfrom(fd,buffer,128,0,(struct sockaddr*)&addr, &addrlen);
+     
+     if(n==-1){//error
+     printf("error: %s\n", strerror(errno));
+     exit(1);
+     }
+     
+     write(1,"echo: ",6);//stdout
+     write(1,buffer,n);
+     close(fd);
+     exit(0);*/
 }
