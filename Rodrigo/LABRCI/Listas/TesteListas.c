60e51ad197d9af2b26ce58bbabf13e717227f4d9
Rodrigo Lopes Do Ó Barbosa <rodrigo.do.o.barbosa@gmail.com>
2017-03-21 23:09:08 +0000

    Todas as Funções para mexer na Lista de Mensagens Implementadas

--- Rodrigo/LABRCI/Listas/TesteListas.c
+++ Rodrigo/LABRCI/Listas/TesteListas.c
@@ -20,49 +20,92 @@
 
 # include <time.h>
 
-
+                    //Estrutura que guarda o Endereço da HEAD e TAIL da lista de Mensagens
+                    //E guarda o numero de mensagens guardadas
 typedef struct List{
-    char message[141];
-    int clock;
-
-    struct List * ptr;
+    struct MSG * head;
+    struct MSG * tail;
+    int size;
+    
 } List_msg;
 
+                    //Estrutura que guar as Mensagens e o sei relogio lógico
+typedef struct MSG{
+    char message[141];
+    int clock;
+    
+    struct MSG * ptr;
+    
+}msg;
 
-
-List_msg * insert(List_msg* head, int num) {
-    List_msg *temp, *prev, *next;
-    temp = (List_msg*)malloc(sizeof(List_msg));
-    temp->clock = num;
-    temp->ptr = NULL;
-    if(!head){
-        head=temp;
-    } else{
-        prev = NULL;
-        next = head;
-        while(next && next->clock<=num){
-            prev = next;
-            next = next->ptr;
-        }
-        if(!next){
-            prev->ptr = temp;
-        } else{
-            if(prev) {
-                temp->ptr = prev->ptr;
-                prev-> ptr = temp;
-            } else {
-                temp->ptr = head;
-                head = temp;
-            }
-        }
+                                            //Função que Regista na Lista as Mensagens
+msg * RegMsg(msg * tail, char * message, int clock ){
+    msg * new;
+    new= (msg*)malloc(sizeof(msg));
+    
+    
+    if  ((((tail->clock)-clock)<0) || (((tail->clock)-clock)==0)){
+        new->clock=tail->clock + 1;
+    }else{
+        new->clock=clock;
     }
-    return head;
+    
+    
+    strcpy(new->message, message);
+    new->ptr=NULL;
+    
+    tail->ptr=new;
+    tail=new;
+
+    return tail;
+}
+
+                        //Função que remove da Lista as mensagens mais antigas
+void RemovMsg(msg * head){
+    msg * old, *new;
+    
+    old = head->ptr;
+    new = old->ptr;
+    head->ptr=new;
+    
+    free(old);
+    
+}
+                                //Função que recebe uma string onde vai escrever TODAS as Mensagens
+void PrintMsg(msg* head, char * message){
+
+    msg * next;
+    next= head->ptr;
+    
+    strcpy(message, "MESSAGES\n");
+    while (next) {
+        strcat(message, next->message);
+        next=next->ptr;
+    }
+}
+
+                            //Função que recebe uma string onde vai escrever as ULTIMAS N Mensagens
+void Print_n_Msg(msg* head, char * message, int n){
+    int i;
+    msg * next;
+    next= head->ptr;
+    
+    for (i=0; i<n; i++) {
+        next= next->ptr;
+    }
+    
+    strcpy(message, "MESSAGES\n");
+    while (next) {
+        strcat(message, next->message);
+        next=next->ptr;
+    }
 }
 
 
-void free_list(List_msg *head) {
-    List_msg *prev = head;
-    List_msg *cur = head;
+                        //FUNÇÃO QUE APAGA LISTA
+void free_list(msg *head) {
+    msg *prev = head;
+    msg *cur = head;
     while(cur) {
         prev = cur;
         cur = prev->ptr;
@@ -71,26 +114,64 @@ void free_list(List_msg *head) {
 }
 
 int main(){
-    int num, count = 0;
-    List_msg *head, *p;
+    
+    char message[141],buffer[141], final[141], final2[141];
+    int clock, count=0;
+    
+    int n;
+    
+    List_msg * imsg;
+    msg *head, *tail;
     head = NULL;
     
-    do {
-        printf("Enter a number");
-        scanf("%d",&num);
-        if(num) {
-            head = insert(head, num);
+                                    //Inicialização da Lista de Mensagens
+    head = (msg *)malloc(sizeof(msg));
+    head->clock=0;
+    head->ptr=NULL;
+    
+    tail=head->ptr;
+                                        // Inicialização da estrutura de memoria da Lista
+    imsg = (List_msg *)malloc(sizeof(List_msg));
+    imsg->head = head;
+    imsg->tail = tail;
+    imsg->size = 0;
+    while (count<6) {
+        
+        fgets(buffer, 141, stdin);
+        
+        sscanf(buffer, "%s %d",message,&clock);
+        
+        
+                                //Verificação e acerto do numero de mensagens
+        if (imsg->size>200) {
+            RemovMsg(imsg->head);
+            imsg->size--;
         }
+                                    //Guardar mensagens
+        if (imsg->tail==NULL) {
+            imsg->tail = RegMsg(imsg->head,message,clock);
+            imsg->size++;
+        }else{
+            imsg->tail = RegMsg(imsg->tail,message,clock);
+            imsg->size++;
+        }
+       // PrintMsg(imsg->head);
         count++;
-    } while(count<4);
-    p = head;
-    printf("\nThe numbers are:\n");
-    while(p) {
-        printf("%d ", p->clock);
-        p = p->ptr;
     }
+
+    fgets(buffer, 141, stdin);
+    
+    sscanf(buffer, "%d",&n);
+    
+                        //Devolve Todas as mensagens da lista
+    PrintMsg(imsg->head, final);
+    
+                        //Devolve as ultimas N mensagens da Lista
+    Print_n_Msg(imsg->head, final2, n);
+    
     free_list(head);
-    return 0;
+    free(imsg);
+        return 0;
 }
 
 
